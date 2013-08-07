5d319700cd429a67d60132b4939383ab511965ce
Pkthunder <jared_perreault@yahoo.com>
2013-08-06 23:37:40 -0400

    Adding core.js to store all globals
    -core.js will store all globals
    -new_app.js will be the new CustomEvent/handler method

--- application.js
+++ application.js
@@ -1,114 +1,5 @@
 $(document).ready( function() {
 
-	/*Pokemon Object/Array*/
-	function Pokemon( name, attack, defense, speed, health, pokedex ) {
-		this.name = name;
-		this.attack = attack;
-		this.defense = defense;
-		this.speed = speed;
-		this.health = health;
-		this.original_health = health;
-		this.pokedex = pokedex;
-		this.player = null;
-		this.other = {};
-		this.status = null;
-		this.disabled = false; //cannot attack this turn if true
-	}
-
-	//Move Object
-	function Move( pwr, acc, effect, pre ) {
-		this.pwr = pwr;
-		this.acc = acc;
-		this.effect = effect;
-		this.pre = pre;
-	}
-
-	//Status Aliment Object
-	//if duration == false, that means infinite
-	function Status( type, duration ) {
-		this.type = type;
-		this.duration = duration;
-	}
-
-	//	2 Player's Global variables
-	var play1 = new Pokemon;
-	var play2 = new Pokemon;
-	var move1 = new Move;
-	var move2 = new Move;
-	/*Global Variable Short List*/
-	var turn = 1;
-	var pick_turn = 1;
-	var nameList = ['Jolteon', 'Vaporeon', 'Flareon', 'Espeon', 'Umbreon', 'Leafeon', 'Glaceon'];
-
-	var creationList = [
-		["Jolteon", 100, 100, 100, 100],
-		["Vaporeon", 100, 100, 100, 100],
-		["Flareon", 100, 100, 100, 100],
-		["Espeon", 100, 100, 100, 100],
-		["Umbreon", 100, 100, 100, 100],
-		["Leafeon", 100, 100, 100, 100],
-		["Glaceon", 100, 100, 100, 100] ];
-
-	var moveList = [ //test values
-		["Yawn", "Discharge", "Charge Beam", "Thunderbolt"],
-		["Wish", "Flail", "Water Pulse", "Auora Beam"],
-		["Facade", "Fire Fang", "Wil-O-Wisp", "Flame Charge"],
-		["Swift", "Calm Mind", "Magic Coat", "Stored Power"],
-		["Taunt", "Curse", "Toxic", "Payback"],
-		["Leaf Blade", "Leach Seed", "Substitute", "Swords Dance"],
-		["Blizzard", "Ice Shard", "Mirror Coat", "Frost Breath"] ];
-
-	//To access: var[Pokemon.pokedex][Move Number][Move Stat Info]
-	//Move Stat Info is organized as follows: var[][][index]
-	//									var[0] = Move Name/Effect
-	//									var[1] = Move Power
-	//									var[2] = Move Accuracy
-	//									var[3] = Pre Effect Boolean
-	var moveParams = [
-		[ //Jolteon - 0
-			["Yawn", null, null, false], //0
-			["Discharge", 80, 1, false], //1
-			["Charge Beam", 50, .9, false], //2
-			["Thunderbolt", 95, 1] ], //3
-
-		[ //Vaporeon - 1
-			["Wish", null, null, false], //0
-			["Flail", 20, 1, true], //1
-			["Water Pulse", 60, 1, false], //2
-			["Auora Beam", 65, 1, false] ], //3
-		
-		[ //Flareon - 2
-			["Facade", 70, 1, true],
-			["Fire Fang", 60, .95, true],
-			["Wil-O-Wisp", null, null, false],
-			["Flame Charge", 50, 1, false] ],
-		
-		[ //Espeon - 3
-			["Swift", null, null, true],
-			["Calm Mind", null, null, false],
-			["Magic Coat", null, null,, true],
-			["Stored Power", 20, 1, true] ],
-		
-		[ //Umbreon - 4
-			["Taunt", null, null, false],
-			["Curse", null, null, false],
-			["Toxic", null, null, false],
-			["Payback", 50, 1, true] ],
-		
-		[ //Leafeon - 5
-			["Leaf Blade", 90, 1, null],
-			["Leach Seed", null, null, false],
-			["Substitute", null, null, true], //check priority
-			["Swords Dance", null, null, false] ],
-			
-		[ //Glaceon - 6
-			["Blizzard", 120, .7, null],
-			["Ice Shard", 40, 1, true],
-			["Mirror Coat", null, null, true], //check priority
-			["Frost Breath", 40, .9, true] ] ];
-
-	/*******************************************************************************************************/
-
 	/*Header Button Functions*/
 
 	/*Mouse over header button functions*/
@@ -166,9 +57,7 @@ $(document).ready( function() {
 			});
 			play2 = setMenu($Evana, $("#p2_info") );
 			play1.player = 2;
-			$(".button").bind("click", function() {
-				button_click( $(this) );
-			});
+			$(".button").each().addClass('move_click');
 		}
 
 		$(this).unbind("click mouseenter mouseleave");
@@ -210,9 +99,7 @@ $(document).ready( function() {
 			$(".random_wrapper").on("mouseleave", function(){
 				$(this).unbind("click mouseenter mouseleave")
 			});
-			$(".button").bind("click", function() {
-				button_click( $(this) );
-			});
+			$(".button").addClass("move_click");
 		}
 
 		//correctly highlights/fades/displays text on the
@@ -254,16 +141,16 @@ $(document).ready( function() {
 	// takes place, or after
 	//Example: Facade is a Pre Effect to increase power if status
 	//Example: Charge Beam is a Post Effect, increases attack AFTER the turn
-	function button_click( CoolWhip ) {
-		var Evana = getPoke(CoolWhip);
-		var Gizzi = getMove(CoolWhip);
+	$(".move_click").on( "click", function() {
+		var Evana = getPoke($(this));
+		var Gizzi = getMove($(this));
 		Gizzi = Gizzi - 1;
 		var tooCute = new Move(	moveParams[Evana][Gizzi][1], moveParams[Evana][Gizzi][2],
 								moveParams[Evana][Gizzi][0], moveParams[Evana][Gizzi][3] );
 		alert(moveParams[Evana][Gizzi][0]);
-		loadMove(tooCute, CoolWhip);
+		loadMove(tooCute, $(this));
 
-	}
+	});
 
 	function getPoke( Gizzi ) {
 		var Evana = Gizzi.parent().attr("id");
@@ -283,11 +170,13 @@ $(document).ready( function() {
 		if ( moveItGurl.pre ) {
 			var Evana = moveItGurl.effect;
 			Evana = Evana.replace(' ', '');
-			moveEffects[Evana](moveItGurl);
+			alert(Evana);
+			moveEffects[eval(Evana)](moveItGurl);
 			console.log("Running pre-move effect");
 		}
 		var Gizzi = datPointer.parent().attr("id");
 		Gizzi = Gizzi.replace('p','').replace('_button_wrapper','');
+		alert("loadMove: var Gizzi: " +Gizzi);
 	}
 
 	/*******************************************************************************************************/
--- move_functions.js
+++ move_functions.js
@@ -1,90 +1,90 @@
 var moveEffects = {
 //Jolteon
 	Yawn: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	Discharge: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	ChargeBeam: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 /////////////////////////////////////////////////
 //Vaporeon
 	Wish: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	Flail: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	WaterPulse: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	AuoraBeam: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 ////////////////////////////////////////////////
 //Flareon
 	Facade: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	FireFang: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	Wisp: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	FlameCharge: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 ///////////////////////////////////////////////
 //Espeon
 	Swift: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	CalmMind: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	MagicCoat: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	StoredPower: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 ///////////////////////////////////////////////
 //Umbreon
 	Taunt: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	Curse: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	Toxic: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	Payback: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 //////////////////////////////////////////////
 //Leafeon
 	LeachSeed: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	Substitute: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	SwordsDance: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 //////////////////////////////////////////////
 //Glaceon
 	IceShard: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	MirrorCoat: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
-	}
+		console.log("Evana Gizzi is an absolute babe");
+	},
 	FrostBreath: function(Beasley) {
-		alert("Evana Gizzi is an absolute babe");
+		console.log("Evana Gizzi is an absolute babe");
 	}
 };
